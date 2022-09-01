34dcf359219b084224f913d3468c127fdb2c8faa
Brandon <brandon.ebarnett@gmail.com>
2022-09-01 04:28:49 -0400

    Add project files.

---
+++ Exercise013/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Write a program and ask the user to continuously enter a number or type "Quit" to exit.
+ * The list of numbers may include duplicates.  Display the unique numbers that the use entered.*/
+
+string userInput = "";
+List<int> numbers = new List<int>();
+
+while (!userInput.ToLower().Equals("quit"))
+{
+    Console.WriteLine("Please enter a number.");
+    userInput = Console.ReadLine();
+    bool validNumber = int.TryParse(userInput, out int result);
+    if (!validNumber)
+        continue;
+    numbers.Add(result);
+}
+PrintUniqueNumbers(numbers);
+
+static void PrintUniqueNumbers(List<int> numbers)
+{
+    List<int> uniqueNumbers = numbers.Distinct().ToList();
+
+    Console.WriteLine(String.Join(", ", uniqueNumbers));
+}
